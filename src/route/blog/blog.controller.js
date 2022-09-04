cc6df25ec4b66c0f99790db548b4c7710f6a0d9b
Assami Muzaki <muzakiassami@gmail.com>
2022-09-04 20:51:31 +0700

    fixing blog frontend

--- src/route/blog/blog.controller.js
+++ src/route/blog/blog.controller.js
@@ -3,9 +3,11 @@ const newsModel = require('../../model/news.mongo')
 async function getDocsById(req,res){
     const paramId = req.params.id
     let doc = await newsModel.findById(paramId)
+    let content = doc.content.split('\n')
     res.render('blog', {
         doc: doc,
-        user: req.user
+        user: req.user,
+        content: content
     })
 }
 
