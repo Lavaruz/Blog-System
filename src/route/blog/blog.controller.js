a9b96e5a323a624db181ae2ab1f900172f05acf4
Assami Muzaki <muzakiassami@gmail.com>
2022-09-06 10:19:48 +0700

    finishing fe

--- src/route/blog/blog.controller.js
+++ src/route/blog/blog.controller.js
@@ -1,12 +1,12 @@
-const {getDocById, getAllDocs} = require('../../model/home.model')
+const {getDocById, getAllDocs, getDocByAutor} = require('../../model/home.model')
 
 async function getDocsById(req,res){
     let doc = await getDocById(req.params.id)
-    let docs = await (await getAllDocs()).slice(0,4)
+    let docByAutor = await getDocByAutor(doc.autor)
     let content = doc.content.split('\n')
     res.render('blog', {
         doc: doc,
-        docs:docs,
+        docByAutor:docByAutor,
         user: req.user,
         content: content
     })
