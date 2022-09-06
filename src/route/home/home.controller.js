a2382ab3af20ea8537f7cdab3e4d68ba353b6b23
Assami Muzaki <muzakiassami@gmail.com>
2022-09-06 09:14:13 +0700

    more update

--- src/route/home/home.controller.js
+++ src/route/home/home.controller.js
@@ -1,16 +1,28 @@
-const {getAllDocs} = require('../../model/home.model')
+const {getAllDocs, getDocByAutor} = require('../../model/home.model')
 
 async function getHome(req,res){
     const docs = await getAllDocs();
-    res.render('home', {
-        news: docs,
-        user: req.user,
-    })
+    const docByAutor = await getDocByAutor(req.user);
+    console.log(docByAutor);
+    if(!req.user){
+        res.render('home', {
+            news: docs,
+            user: req.user,
+        })
+    }else{
+        res.render('home-auth', {
+            news: docs,
+            user: req.user,
+            docByAutor: docByAutor
+        })
+    }
+    
 }   
 
 function logout(req,res){
     req.logout()
     res.redirect('/')
+    
 }
 
 module.exports = {
